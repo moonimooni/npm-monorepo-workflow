0efff32a93eb3ca69008bd69333923ae43abadba
문희원 <munhw1408@gmail.com>
2023-10-22 16:59:34 +0900

    Compare commit-msg hook and prepare-commit-msg hooks.

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,6 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['bar', 'baz', 'common', 'foo']],
-  },
-};
+// module.exports = {
+//   extends: ['@commitlint/config-conventional'],
+//   rules: {
+//     'type-enum': [2, 'always', ['bar', 'baz', 'common', 'foo']],
+//   },
+// };
